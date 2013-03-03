3d3d4b41944264f5409ee3bed0d33f8ed943db0f
Mike Costanza <mike@bandsintown.com>
2013-03-03 11:56:11 -0800

    Initial commit after rails project generation
    $ rails new sensori --database=mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sensori::Application.config.secret_token = '6dbe505d869e3ecd809f7049040a4cb3831a63291ff320cb4ec44a33e4aeb9c5ab7666b76255d59fa5d3b9d4134b95ac1bf65ef85e8617ca8afaf04546637084'
