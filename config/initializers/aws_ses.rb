28ef6e89228e3b0cf191f7aae413a54f60c6cb38
Mike Costanza <mjcostan@gmail.com>
2013-04-03 21:05:52 -0700

    fix typo in SES config

--- config/initializers/aws_ses.rb
+++ config/initializers/aws_ses.rb
@@ -3,5 +3,5 @@
 access_key = ENV['AWS_ACCESS_KEY_ID'].present? ? ENV['AWS_ACCESS_KEY_ID'] : APP_CONFIG['aws']['access_key_id']
 secret_key = ENV['AWS_SECRET_KEY'].present?    ? ENV['AWS_SECRET_KEY']    : APP_CONFIG['aws']['secret_access_key']
 
-ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base, :access_key => access_key, :secret_access_key => secret_key
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base, :access_key_id => access_key, :secret_access_key => secret_key
   
