531ce91d9e3249ba12852194db4968fe939b7361
Mike Costanza <mjcostan@gmail.com>
2013-04-03 20:38:39 -0700

    configure SES with ENV variables when available (production server)

--- config/initializers/aws_ses.rb
+++ config/initializers/aws_ses.rb
@@ -1,3 +1,7 @@
-ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => APP_CONFIG['aws']['access_key_id'],
-  :secret_access_key => APP_CONFIG['aws']['secret_access_key']
+# NOTE: environment variables are set on production server from elastic beanstalk configuration.
+#       use https://console.aws.amazon.com/elasticbeanstalk/home?region=us-west-1 or elastic beanstalk CLI tools to edit.
+access_key = ENV['AWS_ACCESS_KEY_ID'].present? ? ENV['AWS_ACCESS_KEY_ID'] : APP_CONFIG['aws']['access_key_id']
+secret_key = ENV['AWS_SECRET_KEY'].present?    ? ENV['AWS_SECRET_KEY']    : APP_CONFIG['aws']['secret_access_key']
+
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base, :access_key => access_key, :secret_access_key => secret_key
+  
