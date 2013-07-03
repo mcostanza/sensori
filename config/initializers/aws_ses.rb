4871f4a64fa92cbe6c9a2683cb9c6419cff5237d
Mike Costanza <mjcostan@gmail.com>
2013-07-02 17:57:42 -0700

    modified initializers to use APP_CONFIG instead of ENV

--- config/initializers/aws_ses.rb
+++ config/initializers/aws_ses.rb
@@ -1,7 +1,2 @@
-# NOTE: environment variables are set on production server from elastic beanstalk configuration.
-#       use https://console.aws.amazon.com/elasticbeanstalk/home?region=us-west-1 or elastic beanstalk CLI tools to edit.
-access_key = ENV['AWS_ACCESS_KEY_ID'].present? ? ENV['AWS_ACCESS_KEY_ID'] : APP_CONFIG['aws']['access_key_id']
-secret_key = ENV['AWS_SECRET_KEY'].present?    ? ENV['AWS_SECRET_KEY']    : APP_CONFIG['aws']['secret_access_key']
-
-ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base, :access_key_id => access_key, :secret_access_key => secret_key
+ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base, :access_key_id => APP_CONFIG['aws']['access_key_id'], :secret_access_key => APP_CONFIG['aws']['secret_access_key']
   
--- lib/sensori/soundcloud.rb
+++ lib/sensori/soundcloud.rb
@@ -5,11 +5,11 @@ module Sensori
       attr_accessor :client_id, :secret, :app_client
 
       def client_id
-        @client_id ||= ENV['SOUNDCLOUD_CLIENT_ID'].present? ? ENV['SOUNDCLOUD_CLIENT_ID'] : APP_CONFIG['soundcloud']['client_id']
+        @client_id ||= APP_CONFIG['soundcloud']['client_id']
       end
 
       def secret
-        @secret ||= ENV['SOUNDCLOUD_SECRET'].present? ? ENV['SOUNDCLOUD_SECRET'] : APP_CONFIG['soundcloud']['secret']
+        @secret ||= APP_CONFIG['soundcloud']['secret']
       end
 
       def app_client
