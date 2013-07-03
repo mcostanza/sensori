7f6141e0cb502e5e96ecf5d8fbfea1c8e47620ca
Mike Costanza <mjcostan@gmail.com>
2013-07-02 18:31:21 -0700

    fix typo in unicorn init.d script

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -6,7 +6,7 @@ set -e
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/var/www/sensori
-PID=/home/unicorn/tmp/pids/unicorn.pid
+PID=/home/unicorn/pids/unicorn.pid
 CMD="/usr/local/rvm/bin/bootup_unicorn_rails -D -c $APP_ROOT/config/unicorn.rb -E production"
 action="$1"
 set -u
