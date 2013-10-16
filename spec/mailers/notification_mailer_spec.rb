9c564fb5d1255ac42d44feec990c94db6f3492ac
Mike Costanza <mjcostan@gmail.com>
2013-10-15 19:06:03 -0700

    include comment in discussion notifications

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -15,7 +15,7 @@ describe NotificationMailer do
       @member_3 = double(Member, :email => "will@mail.com", :name => "William")
 
       @discussion = double(Discussion, :subject => "check out my neat beat", :member => @member_1)
-      @response   = double(Response, :member => @member_2, :discussion => @discussion)
+      @response   = double(Response, :member => @member_2, :discussion => @discussion, :body => "that beat is dope")
     end
     it "should send an email to the member passed with the correct subject" do
       email = NotificationMailer.discussion_notification(:member => @member_1, :response => @response).deliver
@@ -28,21 +28,24 @@ describe NotificationMailer do
       email = NotificationMailer.discussion_notification(:member => @member_1, :response => @response).deliver
       email.to.should == ["slim@mail.com"]
       email.subject.should == "Five05 posted in a discussion on Sensori"
-      email.encoded.should include("Hey Slim James, Five05 just commented on your post titled \"check out my neat beat\" in Discussions.")
+      email.encoded.should include("Hey Slim James, Five05 just commented on your post titled \"check out my neat beat\" in Discussions:")
+      email.encoded.should include("that beat is dope")
     end
     it "should format the text correctly when the member who created the response also created the discussion" do
       @response.stub(:member).and_return(@member_1)
       email = NotificationMailer.discussion_notification(:member => @member_2, :response => @response).deliver
       email.to.should == ["phil@mail.com"]
       email.subject.should == "Slim James posted in a discussion on Sensori"
-      email.encoded.should include("Hey Five05, Slim James just commented on their post titled \"check out my neat beat\" in Discussions.")
+      email.encoded.should include("Hey Five05, Slim James just commented on their post titled \"check out my neat beat\" in Discussions:")
+      email.encoded.should include("that beat is dope")
     end
     it "should format the text correctly when the member receiving the notification did not create the discussion" do
       @discussion.stub(:member).and_return(@member_3)
       email = NotificationMailer.discussion_notification(:member => @member_1, :response => @response).deliver
       email.to.should == ["slim@mail.com"]
       email.subject.should == "Five05 posted in a discussion on Sensori"
-      email.encoded.should include("Hey Slim James, Five05 just commented on William&#x27;s post titled \"check out my neat beat\" in Discussions.")
+      email.encoded.should include("Hey Slim James, Five05 just commented on William&#x27;s post titled \"check out my neat beat\" in Discussions:")
+      email.encoded.should include("that beat is dope")
     end
   end
 
