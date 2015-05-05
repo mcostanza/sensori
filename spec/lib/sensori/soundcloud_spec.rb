88169792769131e1f4d3583f849941fb1e010124
Mike Costanza <mjcostan@gmail.com>
2015-05-04 18:09:46 -0700

    refactor soundcloud lib spec

--- spec/lib/sensori/soundcloud_spec.rb
+++ spec/lib/sensori/soundcloud_spec.rb
@@ -2,33 +2,33 @@ require 'spec_helper'
 
 describe Sensori::Soundcloud do
   
-  it "should have a module accessor for client_id" do
-    Sensori::Soundcloud.should respond_to(:client_id)
-    Sensori::Soundcloud.should respond_to(:client_id=)
+  it "has a module accessor for client_id" do
+    expect(Sensori::Soundcloud).to respond_to(:client_id)
+    expect(Sensori::Soundcloud).to respond_to(:client_id=)
   end
 
-  it "should have a module accessor for secret" do
-    Sensori::Soundcloud.should respond_to(:secret)
-    Sensori::Soundcloud.should respond_to(:secret=)
+  it "has a module accessor for secret" do
+    expect(Sensori::Soundcloud).to respond_to(:secret)
+    expect(Sensori::Soundcloud).to respond_to(:secret=)
   end
 
-  it "should have a module accessor for app_client" do
-    Sensori::Soundcloud.should respond_to(:app_client)
-    Sensori::Soundcloud.should respond_to(:app_client=)
+  it "has a module accessor for app_client" do
+    expect(Sensori::Soundcloud).to respond_to(:app_client)
+    expect(Sensori::Soundcloud).to respond_to(:app_client=)
   end
 
   describe ".app_client" do
     before(:each) do
-      Sensori::Soundcloud.stub(:client_id).and_return(123)
-      Sensori::Soundcloud.stub(:secret).and_return('secret')
+      allow(Sensori::Soundcloud).to receive(:client_id).and_return(123)
+      allow(Sensori::Soundcloud).to receive(:secret).and_return('secret')
       Sensori::Soundcloud.app_client = nil
     end
-    it "should return a ::Soundcloud instance initialized with client id and secret" do
+
+    it "returns a ::Soundcloud instance initialized with client id and secret" do
       app_client = Sensori::Soundcloud.app_client
-      app_client.should be_an_instance_of(::Soundcloud::Client)
-      app_client.client_id.should == 123
-      app_client.client_secret.should == 'secret'
+      expect(app_client).to be_an_instance_of(::Soundcloud::Client)
+      expect(app_client.client_id).to eq 123
+      expect(app_client.client_secret).to eq 'secret'
     end
   end
-
 end
