e39c9b089af83d2675ce7daa04fcc0dcc8d27ebf
Mike Costanza <mjcostan@gmail.com>
2015-05-31 10:13:49 -0700

    add better error handling to soundcloud track sync service

--- app/services/soundcloud_track_sync_service.rb
+++ app/services/soundcloud_track_sync_service.rb
@@ -2,8 +2,16 @@
 #
 class SoundcloudTrackSyncService
   def run
+  	errors = []
+    
     Member.find_each do |member|
-      member.sync_soundcloud_tracks
+    	begin
+      	member.sync_soundcloud_tracks
+      rescue SoundCloud::ResponseError => ex
+      	errors << ["#{ex.class} - #{ex.message} - #{member.inspect}"]
+      end
     end
+
+    raise errors.join("\n") if errors.any?
   end
 end
